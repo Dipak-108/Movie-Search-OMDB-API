033f5901dfa66e29574017e5ad69d627e2077e4b
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-07-25 21:08:14 +0545

    cut update and paste update feature added

--- src/main.js
+++ src/main.js
@@ -3,22 +3,28 @@ searchDiv = document.getElementsByClassName("search_result_div")[0];
 searchResult = document.getElementsByClassName("search_result")[0];
 timeout = null;
 
-
+//while typing the movie name in search field
 searchInput.addEventListener("keydown", (event) => {
-clearTimeout(timeout);
-
-  timeout = setTimeout(showResult, 300);
-
-  function showResult() {
-    searchDiv.style.display = "block";
-    searchDiv.innerHTML = event.target.value;
-
-    if (event.target.value === "") {
-      searchDiv.style.display = "none";
-    }
-  }
+  showResult(event);
 });
 
+//makes search div display to block if clicked on search bar
+searchInput.addEventListener("click", (event) => {
+  if (searchInput.value.length > 1) {
+    searchDiv.style.display = "block";
+  }
+  showResult(event);
+});
+
+// updates search result while pasting with right click mouse
+searchInput.addEventListener("paste", (event) => {
+  showResult(event);
+});
+
+//updates search result when cut with mouse context menu
+searchInput.addEventListener("cut", (event) => {
+  showResult(event);
+});
 
 //makes search div display to none if clicked anywhere except search bar
 document.addEventListener("click", (event) => {
@@ -27,11 +33,15 @@ document.addEventListener("click", (event) => {
   }
 });
 
-//makes search div display to block if clicked on search bar
-searchInput.addEventListener("click", (event) => {
-  if (searchInput.value.length > 1) {
+function showResult(event) {
+  clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    searchDiv.innerHTML = event.target.value;
     searchDiv.style.display = "block";
-  }
-});
 
-
+    if (event.target.value === "") {
+      searchDiv.style.display = "none";
+    }
+  }, 400);
+}
